5a8a235a8c79f199c6ab1017baf9d99d2719543e
Aiden-Hyun <aiden.hyun.business@gmail.com>
2025-08-22 01:08:23 -0400

    chore: Clean up project structure and stage changes

--- src/features/theme/components/index.ts
+++ src/features/theme/components/index.ts
@@ -1,9 +1,5 @@
 // Export theme components
 
-export { default as BlurView, BlurViewProps } from './BlurView';
-export { default as ClaymorphismDemo } from './ClaymorphismDemo';
-export { default as ClayView, ClayViewProps } from './ClayView';
-export { default as GlassmorphismDemo } from './GlassmorphismDemo';
 export { default as ThemeSelector } from './ThemeSelector';
 export { default as ThemeSettingsSection } from './ThemeSettingsSection';
 
